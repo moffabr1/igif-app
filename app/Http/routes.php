72dfc2600b289c6a448363115a0f3ea899bddbcc
Brian Moffatt <moffabr1@Brians-MBP.ptd.net>
2016-09-22 13:53:37 -0400

    2nd Commit. Got the views from SB Admin 2 wrking with Edwin's project starter

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,10 +11,94 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+/*
+ * Original Route from Edwin's Class
+ */
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+/*
+ * Route from SB Admin Theme
+ */
+
+Route::get('/', function()
+{
+    return View::make('home');
 });
 
 Route::auth();
 
 Route::get('/home', 'HomeController@index');
+
+Route::resource('admin/users', 'AdminUsersController');
+
+
+
+
+
+//Route::get('/', function()
+//{
+//    return View::make('home');
+//});
+
+Route::get('/charts', function()
+{
+    return View::make('mcharts');
+});
+
+Route::get('/tables', function()
+{
+    return View::make('table');
+});
+
+Route::get('/forms', function()
+{
+    return View::make('form');
+});
+
+Route::get('/grid', function()
+{
+    return View::make('grid');
+});
+
+Route::get('/buttons', function()
+{
+    return View::make('buttons');
+});
+
+
+Route::get('/icons', function()
+{
+    return View::make('icons');
+});
+
+Route::get('/panels', function()
+{
+    return View::make('panel');
+});
+
+Route::get('/typography', function()
+{
+    return View::make('typography');
+});
+
+Route::get('/notifications', function()
+{
+    return View::make('notifications');
+});
+
+Route::get('/blank', function()
+{
+    return View::make('blank');
+});
+
+//Route::get('/login', function()
+//{
+//    return View::make('login');
+//});
+
+Route::get('/documentation', function()
+{
+    return View::make('documentation');
+});
